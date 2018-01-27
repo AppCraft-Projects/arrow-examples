e0c8c9d1e2fa39301f666ab7fa7da2d818999db4
adam-arold <arold.adam@gmail.com>
2018-01-27 15:31:28 +0100

    Add simple FP code example

---
+++ src/main/kotlin/org/codetome/arrow/ArrowExtensions.kt
@@ -0,0 +1,9 @@
+package org.codetome.arrow
+
+import arrow.syntax.eq.eqv
+import arrow.syntax.eq.neqv
+import arrow.typeclasses.eq
+
+inline fun <reified F> F.equivalentTo(b: F): Boolean = eqv(eq(), b)
+
+inline fun <reified F> F.notEquivalentTo(b: F): Boolean = neqv(eq(), b)
